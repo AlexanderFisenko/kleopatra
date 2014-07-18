167162ce9bd4402aea1537a0cbd1159d25c162e0
AlexanderFisenko <1alexanderfisenko@gmail.com>
2014-07-18 20:21:39 +0600

    s

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,7 +1,7 @@
 #encoding: UTF-8
 class FeedbackController < ApplicationController
   def index
-    @photos = Photo.where(page_name: "отзывы").order(id: :asc)
+    @photos = Photo.where(page_name: "feedback").order(id: :asc)
     set_meta_tags title: 'Отзывы', description: "Отзывы и комментарии о Банкет холл Клеопатра"
   end
 end
--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,7 +1,7 @@
 #encoding: UTF-8
 class GalleryController < ApplicationController
   def index
-    @photos = Photo.where(page_name: "галерея").order(id: :asc)
+    @photos = Photo.where(page_name: "gallery").order(id: :asc)
     set_meta_tags title: 'Фотогалерея', description: "Фотографии банкетного зала Банкет холл Клеопатра"
   end
 end
