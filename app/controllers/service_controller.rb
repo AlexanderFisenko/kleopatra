b8156785a428b94fa70350b1e9be93e2afba9716
AlexanderFisenko <1alexanderfisenko@gmail.com>
2014-07-18 21:08:35 +0600

    s

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -2,15 +2,15 @@
 class ServiceController < ApplicationController
 
   def order
-  	set_meta_tags title: 'Заказ банкета', description: "Организация и заказ банкета в Магнитогорске"
+    set_meta_tags title: 'Заказ банкета', description: "Организация и заказ банкета в Магнитогорске"
   end
 
   def reservation
-  	set_meta_tags title: 'Бронирование даты', description: "Забронировать банкетный зал в Магнитогорске"
+    set_meta_tags title: 'Бронирование даты', description: "Забронировать банкетный зал в Магнитогорске"
   end
 
   def serving
-  	set_meta_tags title: 'Сервировка', description: "Оформление банкетного зала в Магнитогорске"
+    set_meta_tags title: 'Сервировка', description: "Оформление банкетного зала в Магнитогорске"
   end
 
   def service
